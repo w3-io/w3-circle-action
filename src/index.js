16eceb22d87ecc53229f3ef8e7033e3519aa33ef
Audie Sheridan <audieleon@gmail.com>
2026-04-03 01:00:02 -0500

    Add unhandledRejection suppression back to v2.0

--- src/index.js
+++ src/index.js
@@ -1,2 +1,7 @@
 import { run } from './main.js'
+
+// Suppress stray unhandled rejections from @actions/core internals.
+// Without this, Node.js >= 15 exits with code 1 on any unhandled rejection.
+process.on('unhandledRejection', () => {})
+
 run()
