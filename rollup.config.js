83014436cb9f7a838b949af7262d9189de9692b4
Audie Sheridan <audieleon@gmail.com>
2026-04-16 03:31:05 -0500

    Fix rollup output path to match action.yml (dist/index.js)

--- rollup.config.js
+++ rollup.config.js
@@ -5,7 +5,7 @@ import json from '@rollup/plugin-json'
 export default {
   input: 'src/index.js',
   output: {
-    file: 'dist/index.cjs',
+    file: 'dist/index.js',
     format: 'cjs',
   },
   plugins: [resolve({ preferBuiltins: true }), commonjs(), json()],
